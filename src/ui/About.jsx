e69f17f64fc57ac2e83d07e92dbef5a3a37721ee
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-09-11 00:05:44 +0600

    adjust image

--- src/ui/About.jsx
+++ src/ui/About.jsx
@@ -62,12 +62,12 @@ const About = () => {
               <img
                 src={Sujon}
                 alt="ProfileImage"
-                className="rounded-lg lg:w-full sm:mx-auto h-96 object-contain"
+                className="rounded-lg lg:w-full sm:mx-auto h-[420px] object-left-top"
               />
-              <div className="hidden lg:inline-block absolute w-full h-96 bg-designColor/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300" />
+              <div className="hidden lg:inline-block absolute w-full h-[410px] bg-designColor/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300" />
             </div>
           </div>
-          <div className="hidden lg:inline-flex w-full h-96 border-2 border-designColor rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300" />
+          <div className="hidden lg:inline-flex w-full h-[410px] border-2 border-designColor rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300" />
         </div>
       </div>
     </Container>
